a749dc5bc5cf839b7699c1d000b2ceb3a468c0fb
flystewie <77213085+flystewie@users.noreply.github.com>
2024-10-10 19:26:32 -0400

    Updated Fragments with Intent

--- F24_Lecture6/app/src/main/java/com/example/f24_lecture6/FirstFragment.java
+++ F24_Lecture6/app/src/main/java/com/example/f24_lecture6/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.f24_lecture6;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +25,19 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        TextView txtView = view.findViewById(R.id.textView);
+
+        txtView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view ;
     }
 }
