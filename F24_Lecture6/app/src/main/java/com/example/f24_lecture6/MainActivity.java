0752c18f83bb4ca3f78b37710ff21ead63bff2dc
flystewie <77213085+flystewie@users.noreply.github.com>
2024-10-10 19:19:54 -0400

    Add Fragment Project

---
+++ F24_Lecture6/app/src/main/java/com/example/f24_lecture6/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.f24_lecture6;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOne, btnTwo;
+    LinearLayout linearLayout ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        linearLayout = findViewById(R.id.linearLayout2);
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirstFragment firstFragment = new FirstFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout2, firstFragment);
+                transaction.commit();
+            }
+        });
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SecondFragment secondFragment = new SecondFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.linearLayout2, secondFragment);
+                transaction.commit();
+            }
+        });
+        };
+}
