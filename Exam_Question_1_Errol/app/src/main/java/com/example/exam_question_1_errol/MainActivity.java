6407c20ce443a19c558ea1799cd5aeaaca714de8
flystewie <77213085+flystewie@users.noreply.github.com>
2024-10-17 20:26:35 -0400

    Finished Question 1 delete row layout challenge

--- Exam_Question_1_Errol/app/src/main/java/com/example/exam_question_1_errol/MainActivity.java
+++ Exam_Question_1_Errol/app/src/main/java/com/example/exam_question_1_errol/MainActivity.java
@@ -7,12 +7,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> nameCars = new ArrayList<>(Arrays.asList("Corolla", "Civic", "PathFinder", "7 Series", "A4", "Yukon", "Model 3", "Alto", "E Class", "Range Rover Sport", "Tuscon"));
+        List<String> modelCars = new ArrayList<>(Arrays.asList("Toyota", "Honda", "Nissan", "BMW", "Audi", "GMC", "Tesla", "Suziki", "Mercedes", "Land Rover", "Hyundai"));
+
+        recyclerView = findViewById(R.id.viewRecycle);
+        PrimaryAdapter adapter = new PrimaryAdapter(this, nameCars, modelCars);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- Exam_Question_1_Errol/app/src/main/java/com/example/exam_question_1_errol/PrimaryAdapter.java
+++ Exam_Question_1_Errol/app/src/main/java/com/example/exam_question_1_errol/PrimaryAdapter.java
@@ -24,10 +24,10 @@ public class PrimaryAdapter extends RecyclerView.Adapter<PrimaryAdapter.MyViewHo
     List<String> modelCars;
     LayoutInflater inflater;
 
-    public PrimaryAdapter(Context context, String[] nameCars, String[] modelCars) {
+    public PrimaryAdapter(Context context, List<String> nameCars, List<String> modelCars) {
         this.context = context;
         this.nameCars = nameCars;
-        this.modelCars = nameCars;
+        this.modelCars = modelCars;
         inflater = LayoutInflater.from(context);
     }
 
@@ -43,33 +43,27 @@ public class PrimaryAdapter extends RecyclerView.Adapter<PrimaryAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.nameCar.setText(namePlayers[position]);
-        holder.scorePlayerTextView.setText(scorePlayers[position]);
-        holder.serialNumberTextView.setText(nameCars[position]);
-        holder.playerImageView.setImageResource(imageIcons[position]);
+        holder.nameCar.setText(nameCars.get(position));
+        holder.modelCar.setText(modelCars.get(position));
+        holder.deleteCar.setOnClickListener(tmp -> {
+            int deletePosition = holder.getAdapterPosition();
+            nameCars.remove(deletePosition);
+            modelCars.remove(deletePosition);
 
+            notifyItemRemoved(deletePosition);
+            notifyItemRangeChanged(deletePosition, nameCars.size());
+        });
     }
 
 
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
 
     @Override
     public int getItemCount() {
-        return namePlayers.length;
+        return nameCars.size() ;
     }
 
-
-
-
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView namePlayerTextView ;
-        TextView scorePlayerTextView;
-        TextView serialNumberTextView ;
-        ImageView playerImageView;
         TextView nameCar ;
         TextView modelCar ;
         ImageView deleteCar ;
@@ -80,36 +74,8 @@ public class PrimaryAdapter extends RecyclerView.Adapter<PrimaryAdapter.MyViewHo
             modelCar = view.findViewById(R.id.modelCar);
             deleteCar = view.findViewById(R.id.deleteCar);
 
-            namePlayerTextView.setOnClickListener(tmp -> customDialog("You Clicked Player Name"));
-            scorePlayerTextView.setOnClickListener(tmp -> customDialog("You Clicked Player Score"));
-            serialNumberTextView.setOnClickListener(tmp -> customDialog("You Clicked Player Number"));
-            playerImageView.setOnClickListener(tmp -> customDialog("You Clicked Player Image"));
-
         }
-
     }
 
-    void customDialog(String outputText) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.dialog_layout, null);
-        alertDialog.setView(view);
-
-        TextView clickedDialog = view.findViewById(R.id.clickedDialog);
-        Button btnDismiss = view.findViewById(R.id.btnDismiss);
-
-        clickedDialog.setText(outputText);
-
-        AlertDialog dialog = alertDialog.create();
-        btnDismiss.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "Try another button :) ", Toast.LENGTH_SHORT).show();
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-
-    }
 }
 
