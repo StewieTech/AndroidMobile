de5bd4dce1c4953ceaa7335809bf9f9053c26a51
flystewie <77213085+flystewie@users.noreply.github.com>
2024-11-16 15:24:54 -0500

    FunAdapter Complete, Need to Finish Main Activity

--- FetchApp/app/src/main/java/com/example/fetchapp/FunAdapter.java
+++ FetchApp/app/src/main/java/com/example/fetchapp/FunAdapter.java
@@ -1,35 +1,53 @@
 package com.example.fetchapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FunAdapter extends RecyclerView.Adapter<FunAdapter.MyViewHolder> {
+import java.util.ArrayList;
 
-    public FunAdapter() {
+public class FunAdapter extends RecyclerView.Adapter<FunAdapter.MyViewHolder> {
+    ArrayList<School> schools;
+    Context context;
+
+    public FunAdapter(Context context, ArrayList<School> schools) {
+        this.context = context;
+        this.schools = schools;
     }
 
     @NonNull
     @Override
     public FunAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.row_layout, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FunAdapter.MyViewHolder holder, int position) {
+        holder.schoolTxtView.setText(schools.get(position).getName());
+        holder.websiteTxtView.setText(schools.get(position).getWebPage());
+        holder.stateTxtView.setText(schools.get(position).getStateProvince());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return schools.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView schoolTxtView, websiteTxtView, stateTxtView ;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            schoolTxtView = itemView.findViewById(R.id.SchoolTxtview);
+            websiteTxtView = itemView.findViewById(R.id.websiteTxtView);
+            stateTxtView = itemView.findViewById(R.id.stateTxtView);
+
         }
     }
 }
--- FetchApp/app/src/main/java/com/example/fetchapp/MainActivity.java
+++ FetchApp/app/src/main/java/com/example/fetchapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fetchapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +9,56 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView schoolTxtView ;
+    TextView websiteTxtView ;
+    TextView stateTxtView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void fetchJSONArray() {
+        String urlString = "http://universities.hipolabs.com/search?country=Canada";
+        RequestQueue requestQueue  = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(urlString, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        schoolTxtView.append(jsonObject.getString("name"));
+                        websiteTxtView.append(jsonObject.getString("web_pages"));
+                        stateTxtView.append(jsonObject.getString("state-province"));
+
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
